0c96d53b2ea855db837028b87b3a378fb58a0839
joseacuna <joseacunaacuna@gmail.com>
2014-05-19 11:11:57 -0400

    inicio de la tarea

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: slenderman
+ *
+ * Created on 19 de mayo de 2014, 10:53 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
